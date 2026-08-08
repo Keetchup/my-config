bf036ff2960242ea5031c19b178e4d772b12b1f2
Keetchup <thedoubik@gmail.com>
2026-08-08 02:14:39 +0200

    mux cleanup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,9 @@
 oh-my-posh init pwsh --config ys | Invoke-Expression
+
+if ((Get-Command psmux -ErrorAction SilentlyContinue) -and (-not $env:PSMUX)) {
+    psmux attach-session -t powershell 2>$null
+
+    if ($LASTEXISTCODE -ne 0) {
+        psmux new-session -s powershell
+    }
+}
